e2777c43e4d42c4fe23949f2f81901b90242e227
Sharon142 <culas.s@maristcollege.school.com>
2022-09-27 19:13:03 +1300

    testing code

--- testing code.js
+++ testing code.js
@@ -11,7 +11,8 @@ function validateForm() {
       return false;
     }
   }
-const Question = document.querySelector("#question");//used to link the html coding using '#question'
+  function openNav() {
+    const Question = document.querySelector("#question");//used to link the html coding using '#question'
 const Answer = document.querySelector("#answer");//input from the user
 const Verdict = document.querySelector("#verdict");//determines whether or not the question is correct
 const questionList = ["What is a MITM Attack", "HTTP is safer than HTTPS (True/False)", 
@@ -53,3 +54,4 @@ function nextQuestion() {
     Question.innerHTML = text;
     document.body.style.backgroundColor = "darkturquoise";   
 }
+  }
