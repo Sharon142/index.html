eeb0a68496d2db3ca0d67d2cc726945b85727f72
Sharon142 <culas.s@maristcollege.school.com>
2022-09-28 22:08:06 +1300

    testing code

--- testingCode.js
+++ testingCode.js
@@ -9,8 +9,13 @@ function validateForm() {
   if (x == "" || x == null) {
     alert("Write your name first");
     return false;
-  } 
-  
+  }   
+}
+function validateForm(){
+  name.innerHTML = ""
+  name.innerHTML = "Hello " + userName.value + ", welcome to my quiz.";
+  greetings.style.display ="none"
+  quiz.style.display ="block"  
 }
     const Question = document.querySelector("#question");//used to link the html coding using '#question'
     const Answer = document.querySelector("#answer");//input from the user
