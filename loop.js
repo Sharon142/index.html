03d6d22aa9defce446e5d1b4bc402b59bc1915e2
Sharon142 <culas.s@maristcollege.school.com>
2022-10-20 21:53:05 +1300

    testing code

--- loop.js
+++ loop.js
@@ -1,9 +1,50 @@
 let value = document.querySelector("#value");
 document.body.style.backgroundColor = "Yellow";
 function input(){
-    while (i < 50) {
+    while (i < 10) {
         text += "Value is too small!";
         document.body.style.backgroundColor = "Turquiose";  
         i++;     
       }
 }
+
+public class Main {
+  static int myMethod(int x) {
+    return 5 + x;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(myMethod(3));
+  }
+}
+
+let response = {
+  response: 'number',
+  total: 49,
+  data: {
+    place: 'school',
+    start: '22/01/19',
+    countries: [
+    'Australia',
+    'New Zealand',
+    'Canada'
+  ],
+  cities: {
+    'California',
+    'New York',
+    'Auckland',
+  }
+}
+ 
+};
+
+function removeCountries(responseObj, countries){
+  responseObj.data.countries.pull(countries);
+  return responseObj.data.countries
+}
+
+console.log(removeCountries(response, Canada));
+console.log(response)
+
+
+  
